3c2dc7e6254fe708a73accc278cadb07814a60c6
keenjo <90385673+keenjo@users.noreply.github.com>
2021-10-27 10:38:24 +0200

    long & keys in bert func

--- eval/bert_eval.py
+++ eval/bert_eval.py
@@ -51,17 +51,31 @@ for k, v in results.items():
 # you can copy this code to the bottom of main.ipynb to see the outputs
 
 def bert_score(ref_summ, gen_summ):
+    nlp = spacy.load("fr_core_news_sm")
 
     # Loop to separate long and short summaries from dataset
+    # tried just unpacking the two variables from gen_summ like in rouge_l.py but it wasn't working for me
     long_summs = []
     short_summs = []
     for x in range(len(gen_summ)):
         long_summs.append(gen_summ[x][0])
         short_summs.append(gen_summ[x][1])
+
+    summ_sentences = []
+    summ_cur_sentence = []
+    for summ in ref_summ:
+        summ = nlp(summ)
+        for sent in summ.sents:
+            for token in sent:
+                if not token.text.lower() in STOP_WORDS and not token.is_punct:
+                    summ_cur_sentence.append(token.lemma_)
+            summ_sentences.append(summ_cur_sentence)
+            summ_cur_sentence = []
         
     scorer = BERTScorer(lang='fr', rescale_with_baseline=True)
 
     P_long, R_long, F1_long = scorer.score(long_summs, ref_summ, verbose=True)
+    P_key, R_key, F1_key = scorer.score(short_summs, summ_sentences, verbose=True)
     # P = precision
     # R = recall
     # F1 = F1-score
@@ -70,8 +84,13 @@ def bert_score(ref_summ, gen_summ):
     results["Long precision avg"] = P_long.mean()
     results["Long recall avg"] = R_long.mean()
     results["Long F1-score avg"] = F1_long.mean()
+    results["Keyword precision avg"] = P_key.mean()
+    results["Keyword recall avg"] = R_key.mean()
+    results["Keyword F1-score avg"] = F1_key.mean()
 
     return results
 
 bert_score(ref_summs, gen_summs)
+
+# Will try to get working with function above, so we can see keyword matrix as well
 #scorer.plot_example(gen_summs[0], ref_summs[0])
