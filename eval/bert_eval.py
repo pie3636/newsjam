1fa6adcfc414252ff9df954b1d3f7269445224e3
keenjo <90385673+keenjo@users.noreply.github.com>
2021-11-14 18:34:43 +0100

    fully working version on BERTScore

--- eval/bert_eval.py
+++ eval/bert_eval.py
@@ -7,8 +7,6 @@ import spacy
 from spacy.lang.fr import STOP_WORDS
 
 # BERTScore Implementation
-# I plan on putting all of these into a class later, so we can easily use them in the main file
-# for now, you can copy this code to the bottom of main.ipynb to see the outputs/test it on our data
 
 # *Note* I've tried running the bert_score on one article and it does not seem to work, I think input needs to be a list of sentences
 
@@ -25,28 +23,28 @@ class BERT_Eval:
         using the get_matrix function lower down in this file *
         '''
 
-        #nlp = spacy.load("fr_core_news_sm")
+        nlp = spacy.load("fr_core_news_sm")
 
         # tried just unpacking the two variables from gen_summs but it wasn't working for me, I have no idea why
-        # so I made a for loop to do the same thing
+        # so I made a 'for loop' to do the same thing
         long_summs = []
         short_summs = []
         for x in range(len(gen_summs)):
             long_summs.append(gen_summs[x][0])
             short_summs.append(gen_summs[x][1])
 
-        summ_sentences = []
-        summ_cur_sentence = []
+        individual_summs = []
+        cur_summ = []
         for summ in ref_summs:
             summ = self.nlp(summ)
             for sent in summ.sents:
                 for token in sent:
                     if not token.text.lower() in STOP_WORDS and not token.is_punct:
-                        summ_cur_sentence.append(token.lemma_)
-                summ_sentences.append(summ_cur_sentence)
-                summ_cur_sentence = []
+                        cur_summ.append(token.lemma_)
+            individual_summs.append(cur_summ)
+            cur_summ = []
 
-        key_ref_summs = [' '.join(sent) for sent in summ_sentences]
+        key_ref_summs = [' '.join(sent) for sent in individual_summs]
 
         return long_summs, short_summs, ref_summs, key_ref_summs
 
@@ -63,9 +61,7 @@ class BERT_Eval:
         scorer = BERTScorer(lang='fr', rescale_with_baseline=True)
 
         P_long, R_long, F1_long = scorer.score(long_summs, ref_summs, verbose=True)
-
-        # Commented out because keyword summary evaluation is not currently working
-        #P_key, R_key, F1_key = scorer.score(short_summs, key_ref_summs, verbose=True)
+        P_key, R_key, F1_key = scorer.score(short_summs, key_ref_summs, verbose=True)
         # P = precision
         # R = recall
         # F1 = F1-score
@@ -74,11 +70,9 @@ class BERT_Eval:
         results["Long precision avg"] = P_long.mean()
         results["Long recall avg"] = R_long.mean()
         results["Long F1-score avg"] = F1_long.mean()
-
-        # Commented out because keyword summary evaluation is not currently working
-        #results["Keyword precision avg"] = P_key.mean()
-        #results["Keyword recall avg"] = R_key.mean()
-        #results["Keyword F1-score avg"] = F1_key.mean()
+        results["Keyword precision avg"] = P_key.mean()
+        results["Keyword recall avg"] = R_key.mean()
+        results["Keyword F1-score avg"] = F1_key.mean()
 
         return results
 
@@ -94,15 +88,16 @@ class BERT_Eval:
 
         return matrix
 
-
+'''
 # Example of how functions could be implemented in main.ipynb
 # just don't forget to add class name before each of the functions
 
-# Calling the split_summs function and storing outputs into variables to be used in last two functions
-#long_summs, short_summs, ref_summs, key_ref_sums =  split_summs(gen_summs, ref_summs)
+- Calling the split_summs function and storing outputs into variables to be used in last two functions
+long_summs, short_summs, ref_summs, key_ref_sums =  split_summs(gen_summs, ref_summs)
 
-# Calling the bert_score function
-#bert_score(long_summs, short_summs, ref_summs, key_ref_sums)
+- Calling the bert_score function
+bert_score(long_summs, short_summs, ref_summs, key_ref_sums)
 
-# Calling the get_matrix function
-#get_matrix(long_summs, ref_summs, 4)
+- Calling the get_matrix function
+get_matrix(long_summs, ref_summs, 4) 
+'''
