5c4bf01a51e9e0773a93a5f97e14fadf805aedc8
keenjo <90385673+keenjo@users.noreply.github.com>
2022-02-09 03:38:47 +0100

    add return for non-repeat condition

--- text_post-process.py
+++ text_post-process.py
@@ -7,20 +7,23 @@ def rep_search(text):
         new_text = text[0:len(find[0][0])]
         return print(new_text)
 
-    for x in range(len(text)):
-        exp1 = rf'(\b\S+\b(\s+\b\S+\b[\.]?){{{x}}})\s*(\1(\s*|\.))+'
-        find2 = re.findall(exp1, text)
+    elif not find:
+        for x in range(len(text)):
+            exp1 = rf'(\b\S+\b(\s+\b\S+\b[\.]?){{{x}}})\s*(\1(\s*|\.))+'
+            find2 = re.findall(exp1, text)
 
-        exp2 = rf'(([A-Z]\S+\b)(\s+\b\S+\b){{{x}}}(\s+\b\S+[\.]?))(\1)+'
-        find3 = re.findall(exp2, text)
+            exp2 = rf'(([A-Z]\S+\b)(\s+\b\S+\b){{{x}}}(\s+\b\S+[\.]?))(\1)+'
+            find3 = re.findall(exp2, text)
 
-        if find2:
-            new_text = text[0:len(find2[0][0])]
-            return print(new_text)
-            break
+            if find2:
+                new_text = text[0:len(find2[0][0])]
+                return print(new_text)
+                break
 
+            elif find3:
+                new_text = text[0:len(find3[0][0])]
+                return print(new_text)
+                break
 
-        elif find3:
-            new_text = text[0:len(find3[0][0])]
-            return print(new_text)
-            break
+        else:
+            return print(text)
