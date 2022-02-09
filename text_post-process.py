1d14357680a3f093c1dddfd102e55636cfe1b503
keenjo <90385673+keenjo@users.noreply.github.com>
2022-02-09 14:23:28 +0100

    good working fxn

--- text_post-process.py
+++ text_post-process.py
@@ -1,29 +1,59 @@
 import re
 
+
 def rep_search(text):
-
-    find = re.findall(r'(\b[A-Z][{a-z}\s\.]+)(\1)+', text)
-    if find:
-        new_text = text[0:len(find[0][0])]
-        return print(new_text)
-
-    elif not find:
-        for x in range(len(text)):
-            exp1 = rf'(\b\S+\b(\s+\b\S+\b[\.]?){{{x}}})\s*(\1(\s*|\.))+'
-            find2 = re.findall(exp1, text)
-
-            exp2 = rf'(([A-Z]\S+\b)(\s+\b\S+\b){{{x}}}(\s+\b\S+[\.]?))(\1)+'
-            find3 = re.findall(exp2, text)
-
-            if find2:
-                new_text = text[0:len(find2[0][0])]
-                return print(new_text)
+     
+    for x in range(len(text)):
+        
+        exp = rf'(([A-Z]\S+\b)(\s+\b\S+\b){{{x}}}(\s+\b\S+[\.]?))(\1)+'
+        search = re.search(exp, text)
+        find = re.findall(exp, text)
+        
+        if search:
+            # starting index of the phrase repetitions
+            start = search.span()[0]
+            # area containing single instance of phrase that is repeated
+            middle = start + len(find[0][0])
+            # ending index of the phrase repetitions
+            end = search.span()[1]
+            
+            if start == 0:
+                # If the repeated phrase is at the beginning of the string
+                new_text = text[start:middle] + text[end:]
+                return new_text
                 break
-
-            elif find3:
-                new_text = text[0:len(find3[0][0])]
-                return print(new_text)
+            
+            else:
+                new_text = text[0:middle] + text[end:len(text)]
+                return new_text
                 break
-
-        else:
-            return print(text)
+            
+    for x in range(len(text)):
+        
+        exp2 = rf'(\b\S+\b(\s+\b\S+\b[\.]?){{{x}}})\s*(\1(\s*|\.))+'
+        search2 = re.search(exp2, text)
+        find2 = re.findall(exp2, text)
+        
+        if search2:
+            # starting index of the phrase repetitions
+            start = search2.span()[0]
+            # area containing single instance of phrase that is repeated
+            middle = start + len(find2[0][0])
+            # ending index of the phrase repetitions
+            end = search2.span()[1]
+           
+            if start == 0:
+               # If the repeated phrase is at the beginning of the string
+               new_text = text[start:middle] + ' ' +text[end:]
+               return new_text
+               break
+    
+            else:
+               new_text = text[0:middle] + ' ' +text[end:len(text)]
+               return new_text 
+               break
+           
+    if not search and not search2:
+    # If there are no repetitions in the string
+        return text
+           
